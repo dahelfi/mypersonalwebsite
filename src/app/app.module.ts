1a48c1daccf300e3b5689d65d0a81befeb3b05bc
Daniel Helfenstein <d.helfenstein@mail.de>
2022-01-30 15:18:16 +0100

    implementing a full responsive navbar successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
